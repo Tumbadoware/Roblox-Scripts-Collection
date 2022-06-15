a8129f21f00463184536f36216c180780ac5c080
Tumbadoware <Eternalsarg@gmail.com>
2022-06-15 01:21:31 -0500

    Update 8. MACRO SCRIPT [DA HOOD].lua

--- 8. MACRO SCRIPT [DA HOOD].lua
+++ 8. MACRO SCRIPT [DA HOOD].lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/Tumbadoware/Roblox-Scripts-Collection/main/loadstring%20maker/8.%20MACRO%20SCRIPT%20%5BDA%20HOOD%5D.lua"),true))()
+loadstring(game:HttpGet(("scripts.eternalwhis.ml/main/MSDH.lua"),true))()
