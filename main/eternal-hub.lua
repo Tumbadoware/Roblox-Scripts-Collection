64267e43d5710f8a25950378a807316cd8dc0956
Tumbadoware <Eternalsarg@gmail.com>
2022-06-15 03:41:25 -0500

    Create eternal-hub.lua

---
+++ main/eternal-hub.lua
@@ -0,0 +1,348 @@
+-- Script Made By Chief Keef#8882
+-- Chief Keef#8882 Is The Only Legit Vendor
+
+-- Instances:
+
+local MainGUI = Instance.new("ScreenGui")
+local main = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local Homebutton = Instance.new("TextButton")
+local Frame = Instance.new("Frame")
+local UICorner_2 = Instance.new("UICorner")
+local TextLabel = Instance.new("TextLabel")
+local TextLabel_2 = Instance.new("TextLabel")
+local TextLabel_3 = Instance.new("TextLabel")
+local Scripts = Instance.new("TextButton")
+local Frame_2 = Instance.new("Frame")
+local UICorner_3 = Instance.new("UICorner")
+local TextLabel_4 = Instance.new("TextLabel")
+local ArsenalScript = Instance.new("TextButton")
+local BVV4 = Instance.new("TextButton")
+local BloxScript = Instance.new("TextButton")
+local IY = Instance.new("TextButton")
+local MS2 = Instance.new("TextButton")
+local MSDH = Instance.new("TextButton")
+local piratespeed = Instance.new("TextButton")
+local NLAF = Instance.new("TextButton")
+local NAT = Instance.new("TextButton")
+local AntiAfk = Instance.new("TextButton")
+
+--Properties:
+
+MainGUI.Name = "MainGUI"
+MainGUI.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+MainGUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+main.Name = "main"
+main.Parent = MainGUI
+main.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+main.Position = UDim2.new(0.294748127, 0, 0.236886635, 0)
+main.Size = UDim2.new(0, 477, 0, 287)
+main.Active = true
+main.Draggable = true
+
+UICorner.Parent = main
+
+Homebutton.Name = "Homebutton"
+Homebutton.Parent = main
+Homebutton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Homebutton.BackgroundTransparency = 1.000
+Homebutton.Position = UDim2.new(0, 0, 0.0192926042, 0)
+Homebutton.Size = UDim2.new(0, 93, 0, 19)
+Homebutton.Font = Enum.Font.Jura
+Homebutton.Text = "Home"
+Homebutton.TextColor3 = Color3.fromRGB(255, 255, 255)
+Homebutton.TextScaled = true
+Homebutton.TextSize = 14.000
+Homebutton.TextWrapped = true
+
+Frame.Parent = Homebutton
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.Position = UDim2.new(0, 0, 0.975122571, 0)
+Frame.Size = UDim2.new(0, 477, 0, 262)
+
+UICorner_2.Parent = Frame
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.Position = UDim2.new(0.256372988, 0, 0.0579992384, 0)
+TextLabel.Size = UDim2.new(0, 230, 0, 107)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "Chief Keef#8882's Exploit GUI"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 17.000
+TextLabel.TextWrapped = true
+
+TextLabel_2.Parent = Frame
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.Position = UDim2.new(0.28930819, 0, 0.538167953, 0)
+TextLabel_2.Size = UDim2.new(0, 200, 0, 50)
+TextLabel_2.Font = Enum.Font.SourceSans
+TextLabel_2.Text = "Click Scripts To Get Started!"
+TextLabel_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.TextSize = 17.000
+
+TextLabel_3.Parent = Frame
+TextLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.Position = UDim2.new(0.243186608, 0, 0.729007661, 0)
+TextLabel_3.Size = UDim2.new(0, 257, 0, 50)
+TextLabel_3.Font = Enum.Font.SourceSans
+TextLabel_3.Text = "The Open/Close GUI Key Is Insert"
+TextLabel_3.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_3.TextSize = 20.000
+
+Scripts.Name = "Scripts"
+Scripts.Parent = main
+Scripts.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Scripts.BackgroundTransparency = 1.000
+Scripts.Position = UDim2.new(0.195378155, 0, 0.0160771701, 0)
+Scripts.Size = UDim2.new(0, 93, 0, 19)
+Scripts.Font = Enum.Font.Jura
+Scripts.Text = "Scripts"
+Scripts.TextColor3 = Color3.fromRGB(255, 255, 255)
+Scripts.TextScaled = true
+Scripts.TextSize = 14.000
+Scripts.TextWrapped = true
+
+Frame_2.Parent = Scripts
+Frame_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame_2.Position = UDim2.new(-1.0128535, 0, 0.975122571, 0)
+Frame_2.Size = UDim2.new(0, 476, 0, 263)
+Frame_2.Visible = false
+
+UICorner_3.Parent = Frame_2
+
+TextLabel_4.Parent = Frame_2
+TextLabel_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_4.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_4.Position = UDim2.new(0.523109257, 0, 0.0301556941, 0)
+TextLabel_4.Size = UDim2.new(0, 209, 0, 30)
+TextLabel_4.Font = Enum.Font.SourceSans
+TextLabel_4.Text = "Scripts Tab 1 | Made By Chief Keef#8882"
+TextLabel_4.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_4.TextScaled = true
+TextLabel_4.TextSize = 12.000
+TextLabel_4.TextWrapped = true
+
+ArsenalScript.Name = "Arsenal-Script"
+ArsenalScript.Parent = Frame_2
+ArsenalScript.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ArsenalScript.Position = UDim2.new(0.258403361, 0, 0.14176245, 0)
+ArsenalScript.Size = UDim2.new(0, 89, 0, 37)
+ArsenalScript.Font = Enum.Font.SourceSans
+ArsenalScript.Text = "Arsenal Script"
+ArsenalScript.TextColor3 = Color3.fromRGB(0, 0, 0)
+ArsenalScript.TextSize = 14.000
+ArsenalScript.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/HonestlyDex/DexHub/main/V3.lua"))()
+end)
+
+
+BVV4.Name = "BVV4"
+BVV4.Parent = Frame_2
+BVV4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+BVV4.Position = UDim2.new(0.486685634, 0, 0.143027663, 0)
+BVV4.Size = UDim2.new(0, 100, 0, 36)
+BVV4.Font = Enum.Font.SourceSans
+BVV4.Text = "Bedwars Vape V4"
+BVV4.TextColor3 = Color3.fromRGB(0, 0, 0)
+BVV4.TextSize = 14.000
+BVV4.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
+end)
+
+
+BloxScript.Name = "BloxScript"
+BloxScript.Parent = Frame_2
+BloxScript.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+BloxScript.Position = UDim2.new(0.741353452, 0, 0.140867829, 0)
+BloxScript.Size = UDim2.new(0, 94, 0, 35)
+BloxScript.Font = Enum.Font.SourceSans
+BloxScript.Text = "Bloxburg GUI"
+BloxScript.TextColor3 = Color3.fromRGB(0, 0, 0)
+BloxScript.TextSize = 14.000
+BloxScript.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/PainfulDestroyer/Roblox/main/Scar%20Hub"))()
+end)
+
+
+IY.Name = "IY"
+IY.Parent = Frame_2
+IY.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+IY.Position = UDim2.new(0.0546218492, 0, 0.348659009, 0)
+IY.Size = UDim2.new(0, 120, 0, 32)
+IY.Font = Enum.Font.SourceSans
+IY.Text = "Infinity Yield(ADMIN)"
+IY.TextColor3 = Color3.fromRGB(0, 0, 0)
+IY.TextSize = 14.000
+IY.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+end)
+
+
+MS2.Name = "MS2"
+MS2.Parent = Frame_2
+MS2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+MS2.Position = UDim2.new(0.366426647, 0, 0.519905925, 0)
+MS2.Size = UDim2.new(0, 99, 0, 32)
+MS2.Font = Enum.Font.SourceSans
+MS2.Text = "Mining Sim 2"
+MS2.TextColor3 = Color3.fromRGB(0, 0, 0)
+MS2.TextSize = 14.000
+MS2.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/TSQ-new/KelpHub/main/Mining%20Simulator%202"))()
+end)
+
+
+MSDH.Name = "MSDH"
+MSDH.Parent = Frame_2
+MSDH.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+MSDH.Position = UDim2.new(0.0556373596, 0, 0.519905925, 0)
+MSDH.Size = UDim2.new(0, 120, 0, 32)
+MSDH.Font = Enum.Font.SourceSans
+MSDH.Text = "Macro Script[DA HOOD]"
+MSDH.TextColor3 = Color3.fromRGB(0, 0, 0)
+MSDH.TextSize = 14.000
+MSDH.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet("http://scripts.eternalwhis.ml/main/MSDH.lua"))()
+end)
+
+piratespeed.Name = "pirate-speed"
+piratespeed.Parent = Frame_2
+piratespeed.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+piratespeed.Position = UDim2.new(0.35172075, 0, 0.345488846, 0)
+piratespeed.Size = UDim2.new(0, 260, 0, 32)
+piratespeed.Font = Enum.Font.SourceSans
+piratespeed.Text = "Legends Of Speed AutoFarm [CANT BE STOPPED]"
+piratespeed.TextColor3 = Color3.fromRGB(0, 0, 0)
+piratespeed.TextSize = 14.000
+piratespeed.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet("http://scripts.eternalwhis.ml/main/pirate-speed.lua"))
+end)
+
+
+NLAF.Name = "NLAF"
+NLAF.Parent = Frame_2
+NLAF.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+NLAF.Position = UDim2.new(0.615546227, 0, 0.517241359, 0)
+NLAF.Size = UDim2.new(0, 95, 0, 32)
+NLAF.Font = Enum.Font.SourceSans
+NLAF.Text = "Ninja Legends AF"
+NLAF.TextColor3 = Color3.fromRGB(0, 0, 0)
+NLAF.TextSize = 14.000
+NLAF.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/TrixAde/Proxima-Hub/main/Main.lua"))()
+end)
+
+
+NAT.Name = "NAT"
+NAT.Parent = Frame_2
+NAT.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+NAT.Position = UDim2.new(0.0526358336, 0, 0.702082992, 0)
+NAT.Size = UDim2.new(0, 120, 0, 37)
+NAT.Font = Enum.Font.SourceSans
+NAT.Text = "Noob Army Tycoon"
+NAT.TextColor3 = Color3.fromRGB(0, 0, 0)
+NAT.TextSize = 14.000
+NAT.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Wxer/roblox/main/random_scripts/noob_army_tycoon.lua"))();
+end)
+
+
+AntiAfk.Name = "AntiAfk"
+AntiAfk.Parent = Frame_2
+AntiAfk.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+AntiAfk.Position = UDim2.new(0.0526358336, 0, 0.140867859, 0)
+AntiAfk.Size = UDim2.new(0, 78, 0, 37)
+AntiAfk.Font = Enum.Font.SourceSans
+AntiAfk.Text = "Anti Afk"
+AntiAfk.TextColor3 = Color3.fromRGB(0, 0, 0)
+AntiAfk.TextSize = 14.000
+AntiAfk.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/KazeOnTop/Rice-Anti-Afk/main/Wind", true))()
+end)
+
+
+-- Scripts:
+
+local function SUXCVXZ_fake_script() -- Homebutton.LocalScript 
+	local script = Instance.new('LocalScript', Homebutton)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		script.Parent.Frame.Visible = true
+		script.Parent.Parent.Scripts.Frame.Visible = false
+	end)
+end
+coroutine.wrap(SUXCVXZ_fake_script)()
+local function FMDQK_fake_script() -- Scripts.LocalScript 
+	local script = Instance.new('LocalScript', Scripts)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		script.Parent.Frame.Visible = true
+		script.Parent.Parent.Homebutton.Frame.Visible = false
+	end)
+end
+coroutine.wrap(FMDQK_fake_script)()
+local function LKJQANP_fake_script() -- main.LocalScript 
+	local script = Instance.new('LocalScript', main)
+
+	--Don't forget to subscribe for more!
+	--Ask your questions in the comments from: https://youtu.be/aCs3f-nVWrE
+	--Tutorial by BloxianCode
+	local UIS = game:GetService("UserInputService")
+	
+	local draggableFrame = script.Parent
+	
+	local IsDragging = false 	--checks if the player is dragging the frame
+	local dragInput				--input variable which we will use later
+	local StartingPoint
+	local oldPos				--Old position from the frame
+	
+	local function update(input)
+		local delta = input.Position - StartingPoint
+		draggableFrame.Position = UDim2.new(oldPos.X.Scale, oldPos.X.Offset + delta.X, oldPos.Y.Scale, oldPos.Y.Offset + delta.Y)
+	end
+	
+	draggableFrame.InputBegan:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseButton1 then
+			IsDragging = true
+			StartingPoint = input.Position
+			oldPos = draggableFrame.Position
+	
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					IsDragging = false
+				end
+			end)
+		end
+	end)
+	
+	draggableFrame.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement then
+			dragInput = input
+		end
+	end)
+	
+	UIS.InputChanged:Connect(function(input)
+		if input == dragInput and IsDragging then
+			update(input)
+		end
+	end)
+end
+coroutine.wrap(LKJQANP_fake_script)()
+local function JHIGTH_fake_script() -- MainGUI.LocalScript 
+	local script = Instance.new('LocalScript', MainGUI)
+
+	function onKeyPress(inputObject, gameProcessedEvent)
+		if not gameProcessedEvent then
+	    if inputObject.KeyCode == Enum.KeyCode.Insert  then 
+			script.Parent.main.Visible = not script.Parent.main.Visible
+			end
+		end
+	end
+	game:GetService("UserInputService").InputBegan:connect(onKeyPress)
+end
+coroutine.wrap(JHIGTH_fake_script)()
