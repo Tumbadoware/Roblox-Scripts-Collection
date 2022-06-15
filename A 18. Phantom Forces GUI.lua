70292e30738172280a7974738d045ac467acb334
Tumbadoware <Eternalsarg@gmail.com>
2022-06-15 01:22:59 -0500

    Update A 18. Phantom Forces GUI.lua

--- A 18. Phantom Forces GUI.lua
+++ A 18. Phantom Forces GUI.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/Tumbadoware/Roblox-Scripts-Collection/main/loadstring%20maker/A%2018.%20Phantom%20Forces%20GUI.lua"),true))()
+loadstring(game:HttpGet(("http://scripts.eternalwhis.ml/main/pirate-forces.lua"),true))()
