46e608c31e915ab42ff3f8357d9d019132da9d0c
Tumbadoware <Eternalsarg@gmail.com>
2022-06-15 01:20:11 -0500

    Update 7. Legends Of Speed - Auto Gain Speed [OP].lua

--- 7. Legends Of Speed - Auto Gain Speed [OP].lua
+++ 7. Legends Of Speed - Auto Gain Speed [OP].lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/Tumbadoware/Roblox-Scripts-Collection/main/loadstring%20maker/7.%20Legends%20Of%20Speed%20-%20Auto%20Gain%20Speed%20%5BOP%5D.lua"),true))()
+loadstring(game:HttpGet(("http://scripts.eternalwhis.ml/main/pirate-speed.lua"),true))()
