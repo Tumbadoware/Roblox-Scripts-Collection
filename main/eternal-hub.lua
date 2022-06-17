8fb943c91bcd24ab207a5d3cdc963e78ef61d4e3
Tumbadoware <Eternalsarg@gmail.com>
2022-06-17 00:17:58 -0500

    Update eternal-hub.lua

--- main/eternal-hub.lua
+++ main/eternal-hub.lua
@@ -26,6 +26,26 @@ local piratespeed = Instance.new("TextButton")
 local NLAF = Instance.new("TextButton")
 local NAT = Instance.new("TextButton")
 local AntiAfk = Instance.new("TextButton")
+local SADH = Instance.new("TextButton")
+local Skywars = Instance.new("TextButton")
+local TextLabel_5 = Instance.new("TextLabel")
+local Scripts2 = Instance.new("TextButton")
+local Frame_3 = Instance.new("Frame")
+local UICorner_4 = Instance.new("UICorner")
+local TextLabel_6 = Instance.new("TextLabel")
+local YLIFE = Instance.new("TextButton")
+local ZAAF = Instance.new("TextButton")
+local EGAF = Instance.new("TextButton")
+local pirateforces = Instance.new("TextButton")
+local CROG = Instance.new("TextButton")
+local FTM = Instance.new("TextButton")
+local BIE = Instance.new("TextButton")
+local WBG = Instance.new("TextButton")
+local PSX = Instance.new("TextButton")
+local TLAF = Instance.new("TextButton")
+local CWG = Instance.new("TextButton")
+local BloxFruits = Instance.new("TextButton")
+local TextLabel_7 = Instance.new("TextLabel")
 
 --Properties:
 
@@ -109,8 +129,8 @@ Scripts.TextWrapped = true
 
 Frame_2.Parent = Scripts
 Frame_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Frame_2.Position = UDim2.new(-1.0128535, 0, 0.975122571, 0)
-Frame_2.Size = UDim2.new(0, 476, 0, 263)
+Frame_2.Position = UDim2.new(-1.00210083, 0, 0.975122571, 0)
+Frame_2.Size = UDim2.new(0, 477, 0, 263)
 Frame_2.Visible = false
 
 UICorner_3.Parent = Frame_2
@@ -140,7 +160,6 @@ ArsenalScript.MouseButton1Down:Connect(function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/HonestlyDex/DexHub/main/V3.lua"))()
 end)
 
-
 BVV4.Name = "BVV4"
 BVV4.Parent = Frame_2
 BVV4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
@@ -151,9 +170,8 @@ BVV4.Text = "Bedwars Vape V4"
 BVV4.TextColor3 = Color3.fromRGB(0, 0, 0)
 BVV4.TextSize = 14.000
 BVV4.MouseButton1Down:Connect(function()
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDad/VapeV4ForRoblox/main/NewMainScript.lua", true))()
 end)
-
 
 BloxScript.Name = "BloxScript"
 BloxScript.Parent = Frame_2
@@ -168,7 +186,6 @@ BloxScript.MouseButton1Down:Connect(function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/PainfulDestroyer/Roblox/main/Scar%20Hub"))()
 end)
 
-
 IY.Name = "IY"
 IY.Parent = Frame_2
 IY.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
@@ -182,11 +199,10 @@ IY.MouseButton1Down:Connect(function()
 	loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 end)
 
-
 MS2.Name = "MS2"
 MS2.Parent = Frame_2
 MS2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-MS2.Position = UDim2.new(0.366426647, 0, 0.519905925, 0)
+MS2.Position = UDim2.new(0.35172075, 0, 0.519905925, 0)
 MS2.Size = UDim2.new(0, 99, 0, 32)
 MS2.Font = Enum.Font.SourceSans
 MS2.Text = "Mining Sim 2"
@@ -195,7 +211,6 @@ MS2.TextSize = 14.000
 MS2.MouseButton1Down:Connect(function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/TSQ-new/KelpHub/main/Mining%20Simulator%202"))()
 end)
-
 
 MSDH.Name = "MSDH"
 MSDH.Parent = Frame_2
@@ -207,7 +222,7 @@ MSDH.Text = "Macro Script[DA HOOD]"
 MSDH.TextColor3 = Color3.fromRGB(0, 0, 0)
 MSDH.TextSize = 14.000
 MSDH.MouseButton1Down:Connect(function()
-	loadstring(game:HttpGet("http://scripts.eternalwhis.ml/main/MSDH.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/TSQ-new/KelpHub/main/Mining%20Simulator%202"))()
 end)
 
 piratespeed.Name = "pirate-speed"
@@ -216,18 +231,17 @@ piratespeed.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 piratespeed.Position = UDim2.new(0.35172075, 0, 0.345488846, 0)
 piratespeed.Size = UDim2.new(0, 260, 0, 32)
 piratespeed.Font = Enum.Font.SourceSans
-piratespeed.Text = "Legends Of Speed AutoFarm [CANT BE STOPPED]"
+piratespeed.Text = "Legends Of Speed AutoFarm [CANT BE STOPED]"
 piratespeed.TextColor3 = Color3.fromRGB(0, 0, 0)
 piratespeed.TextSize = 14.000
 piratespeed.MouseButton1Down:Connect(function()
 	loadstring(game:HttpGet("http://scripts.eternalwhis.ml/main/pirate-speed.lua"))
 end)
 
-
 NLAF.Name = "NLAF"
 NLAF.Parent = Frame_2
 NLAF.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-NLAF.Position = UDim2.new(0.615546227, 0, 0.517241359, 0)
+NLAF.Position = UDim2.new(0.60504204, 0, 0.517241359, 0)
 NLAF.Size = UDim2.new(0, 95, 0, 32)
 NLAF.Font = Enum.Font.SourceSans
 NLAF.Text = "Ninja Legends AF"
@@ -236,7 +250,6 @@ NLAF.TextSize = 14.000
 NLAF.MouseButton1Down:Connect(function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/TrixAde/Proxima-Hub/main/Main.lua"))()
 end)
-
 
 NAT.Name = "NAT"
 NAT.Parent = Frame_2
@@ -251,7 +264,6 @@ NAT.MouseButton1Down:Connect(function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/Wxer/roblox/main/random_scripts/noob_army_tycoon.lua"))();
 end)
 
-
 AntiAfk.Name = "AntiAfk"
 AntiAfk.Parent = Frame_2
 AntiAfk.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
@@ -265,28 +277,263 @@ AntiAfk.MouseButton1Down:Connect(function()
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/KazeOnTop/Rice-Anti-Afk/main/Wind", true))()
 end)
 
+SADH.Name = "SADH"
+SADH.Parent = Frame_2
+SADH.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+SADH.Position = UDim2.new(0.35084033, 0, 0.702082992, 0)
+SADH.Size = UDim2.new(0, 121, 0, 36)
+SADH.Font = Enum.Font.SourceSans
+SADH.Text = "Silent Aim [DA HOOD]"
+SADH.TextColor3 = Color3.fromRGB(0, 0, 0)
+SADH.TextSize = 14.000
+SADH.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Stefanuk12/ROBLOX/master/Games/Da%20Hood/SilentAimAimLock.lua"))()
+end)
+
+Skywars.Name = "Skywars"
+Skywars.Parent = Frame_2
+Skywars.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Skywars.Position = UDim2.new(0.636495173, 0, 0.69740659, 0)
+Skywars.Size = UDim2.new(0, 88, 0, 37)
+Skywars.Font = Enum.Font.SourceSans
+Skywars.Text = "Skywars Script"
+Skywars.TextColor3 = Color3.fromRGB(0, 0, 0)
+Skywars.TextSize = 14.000
+Skywars.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/2dgeneralspam1/scripts-and-stuff/master/scripts/Loadstring9VHD6Wmyi82n", true))()
+end)
+
+TextLabel_5.Parent = Frame_2
+TextLabel_5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_5.Position = UDim2.new(0.35084033, 0, 0.878327012, 0)
+TextLabel_5.Size = UDim2.new(0, 200, 0, 25)
+TextLabel_5.Font = Enum.Font.SourceSans
+TextLabel_5.Text = "Click \"Scripts 2\" to continue"
+TextLabel_5.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_5.TextSize = 14.000
+
+Scripts2.Name = "Scripts2"
+Scripts2.Parent = main
+Scripts2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Scripts2.BackgroundTransparency = 1.000
+Scripts2.Position = UDim2.new(0.390346706, 0, 0.0160771701, 0)
+Scripts2.Size = UDim2.new(0, 93, 0, 19)
+Scripts2.Font = Enum.Font.Jura
+Scripts2.Text = "Scripts 2."
+Scripts2.TextColor3 = Color3.fromRGB(255, 255, 255)
+Scripts2.TextScaled = true
+Scripts2.TextSize = 14.000
+Scripts2.TextWrapped = true
+
+Frame_3.Parent = Scripts2
+Frame_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame_3.Position = UDim2.new(-1.99134815, 0, 1.02775419, 0)
+Frame_3.Size = UDim2.new(0, 476, 0, 261)
+Frame_3.Visible = false
+
+UICorner_4.Parent = Frame_3
+
+TextLabel_6.Parent = Frame_3
+TextLabel_6.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_6.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_6.Position = UDim2.new(0.523109257, 0, 0.0301556941, 0)
+TextLabel_6.Size = UDim2.new(0, 209, 0, 30)
+TextLabel_6.Font = Enum.Font.SourceSans
+TextLabel_6.Text = "Scripts Tab 2 | Made By Chief Keef#8882"
+TextLabel_6.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_6.TextScaled = true
+TextLabel_6.TextSize = 12.000
+TextLabel_6.TextWrapped = true
+
+YLIFE.Name = "YLIFE"
+YLIFE.Parent = Frame_3
+YLIFE.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+YLIFE.Position = UDim2.new(0.258403361, 0, 0.14176245, 0)
+YLIFE.Size = UDim2.new(0, 89, 0, 37)
+YLIFE.Font = Enum.Font.SourceSans
+YLIFE.Text = "Youtube Life"
+YLIFE.TextColor3 = Color3.fromRGB(0, 0, 0)
+YLIFE.TextSize = 14.000
+YLIFE.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Scripts/main/YouTube%20Life/Auto%20Farm.lua"))()
+end)
+
+ZAAF.Name = "ZAAF"
+ZAAF.Parent = Frame_3
+ZAAF.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ZAAF.Position = UDim2.new(0.486685634, 0, 0.143027663, 0)
+ZAAF.Size = UDim2.new(0, 100, 0, 36)
+ZAAF.Font = Enum.Font.SourceSans
+ZAAF.Text = "Zombie Attack AF"
+ZAAF.TextColor3 = Color3.fromRGB(0, 0, 0)
+ZAAF.TextSize = 14.000
+ZAAF.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet("http://scripts.eternalwhis.ml/main/ZAAF.lua"))()
+end)
+
+EGAF.Name = "EGAF"
+EGAF.Parent = Frame_3
+EGAF.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+EGAF.Position = UDim2.new(0.741353452, 0, 0.140867829, 0)
+EGAF.Size = UDim2.new(0, 94, 0, 35)
+EGAF.Font = Enum.Font.SourceSans
+EGAF.Text = "Epic Minigames"
+EGAF.TextColor3 = Color3.fromRGB(0, 0, 0)
+EGAF.TextSize = 14.000
+EGAF.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/SlamminPig/rblxgames/main/Epic%20Minigames/EpicMinigamesGUI"))()
+end)
+
+pirateforces.Name = "pirate-forces"
+pirateforces.Parent = Frame_3
+pirateforces.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+pirateforces.Position = UDim2.new(0.0525210127, 0, 0.348659009, 0)
+pirateforces.Size = UDim2.new(0, 120, 0, 32)
+pirateforces.Font = Enum.Font.SourceSans
+pirateforces.Text = "Phantom Forces GUI"
+pirateforces.TextColor3 = Color3.fromRGB(0, 0, 0)
+pirateforces.TextSize = 14.000
+pirateforces.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet("http://scripts.eternalwhis.ml/main/pirate-forces.lua"))()
+end)
+
+CROG.Name = "CROG"
+CROG.Parent = Frame_3
+CROG.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+CROG.Position = UDim2.new(0.0534014031, 0, 0.523708224, 0)
+CROG.Size = UDim2.new(0, 119, 0, 32)
+CROG.Font = Enum.Font.SourceSans
+CROG.Text = "Chicago Remastered"
+CROG.TextColor3 = Color3.fromRGB(0, 0, 0)
+CROG.TextSize = 14.000
+CROG.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet(('https://raw.githubusercontent.com/lobox920/Shar-Script/main/Chicago%20Remastered.lua')))()
+end)
+
+FTM.Name = "FTM"
+FTM.Parent = Frame_3
+FTM.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+FTM.Position = UDim2.new(0.597654104, 0, 0.333594143, 0)
+FTM.Size = UDim2.new(0, 120, 0, 32)
+FTM.Font = Enum.Font.SourceSans
+FTM.Text = "Find The Markers GUI"
+FTM.TextColor3 = Color3.fromRGB(0, 0, 0)
+FTM.TextSize = 14.000
+FTM.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet('https://pastebin.com/raw/7gu2ztpP'))()
+end)
+
+BIE.Name = "BIE"
+BIE.Parent = Frame_3
+BIE.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+BIE.Position = UDim2.new(0.349619865, 0, 0.334082007, 0)
+BIE.Size = UDim2.new(0, 99, 0, 32)
+BIE.Font = Enum.Font.SourceSans
+BIE.Text = "Bakon Item ESP"
+BIE.TextColor3 = Color3.fromRGB(0, 0, 0)
+BIE.TextSize = 14.000
+BIE.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/HackerDan69/scripts/main/bakon_itemesp"))()
+end)
+
+WBG.Name = "WBG"
+WBG.Parent = Frame_3
+WBG.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+WBG.LayoutOrder = 5
+WBG.Position = UDim2.new(0.348739505, 0, 0.513439119, 0)
+WBG.Size = UDim2.new(0, 95, 0, 32)
+WBG.Font = Enum.Font.SourceSans
+WBG.Text = "Word Bomb GUI"
+WBG.TextColor3 = Color3.fromRGB(0, 0, 0)
+WBG.TextSize = 14.000
+WBG.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/x114/RobloxScripts/main/WordBombSrc"))()
+end)
+
+PSX.Name = "PSX"
+PSX.Parent = Frame_3
+PSX.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+PSX.Position = UDim2.new(0.596753478, 0, 0.51577121, 0)
+PSX.Size = UDim2.new(0, 120, 0, 37)
+PSX.Font = Enum.Font.SourceSans
+PSX.Text = "Pet Simulator X"
+PSX.TextColor3 = Color3.fromRGB(0, 0, 0)
+PSX.TextSize = 14.000
+PSX.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/ExtremeAntonis/KeySystemUI/main/KeySystemUI-Obfuscated.lua"))()
+end)
+
+TLAF.Name = "TLAF"
+TLAF.Parent = Frame_3
+TLAF.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TLAF.Position = UDim2.new(0.0526358336, 0, 0.140867859, 0)
+TLAF.Size = UDim2.new(0, 78, 0, 37)
+TLAF.Font = Enum.Font.SourceSans
+TLAF.Text = "Thick Legends"
+TLAF.TextColor3 = Color3.fromRGB(0, 0, 0)
+TLAF.TextSize = 14.000
+TLAF.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet(("http://scripts.eternalwhis.ml/main/TLAF.lua")))() 
+end)
+
+CWG.Name = "CWG"
+CWG.Parent = Frame_3
+CWG.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+CWG.Position = UDim2.new(0.287815124, 0, 0.709687531, 0)
+CWG.Size = UDim2.new(0, 121, 0, 36)
+CWG.Font = Enum.Font.SourceSans
+CWG.Text = "Combat Warrior GUI"
+CWG.TextColor3 = Color3.fromRGB(0, 0, 0)
+CWG.TextSize = 14.000
+CWG.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet("https://projecthook.xyz/scripts/free.lua"))()
+end)
+
+BloxFruits.Name = "BloxFruits"
+BloxFruits.Parent = Frame_3
+BloxFruits.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+BloxFruits.Position = UDim2.new(0.0650665537, 0, 0.70881331, 0)
+BloxFruits.Size = UDim2.new(0, 84, 0, 37)
+BloxFruits.Font = Enum.Font.SourceSans
+BloxFruits.Text = "BloxFruits GUI"
+BloxFruits.TextColor3 = Color3.fromRGB(0, 0, 0)
+BloxFruits.TextSize = 14.000
+BloxFruits.MouseButton1Down:Connect(function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI'))()
+end)
+
+TextLabel_7.Parent = Frame_3
+TextLabel_7.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_7.Position = UDim2.new(0.348739475, 0, 0.874495625, 0)
+TextLabel_7.Size = UDim2.new(0, 200, 0, 25)
+TextLabel_7.Font = Enum.Font.SourceSans
+TextLabel_7.Text = "More Scripts Weekly!"
+TextLabel_7.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_7.TextSize = 14.000
 
 -- Scripts:
 
-local function SUXCVXZ_fake_script() -- Homebutton.LocalScript 
+local function RINOJE_fake_script() -- Homebutton.LocalScript 
 	local script = Instance.new('LocalScript', Homebutton)
 
 	script.Parent.MouseButton1Click:Connect(function()
 		script.Parent.Frame.Visible = true
 		script.Parent.Parent.Scripts.Frame.Visible = false
+		script.Parent.Parent.Scripts2.Frame.Visible = false
 	end)
 end
-coroutine.wrap(SUXCVXZ_fake_script)()
-local function FMDQK_fake_script() -- Scripts.LocalScript 
+coroutine.wrap(RINOJE_fake_script)()
+local function HSDLQJF_fake_script() -- Scripts.LocalScript 
 	local script = Instance.new('LocalScript', Scripts)
 
 	script.Parent.MouseButton1Click:Connect(function()
 		script.Parent.Frame.Visible = true
 		script.Parent.Parent.Homebutton.Frame.Visible = false
+		script.Parent.Parent.Scripts2.Frame.Visible = false
 	end)
 end
-coroutine.wrap(FMDQK_fake_script)()
-local function LKJQANP_fake_script() -- main.LocalScript 
+coroutine.wrap(HSDLQJF_fake_script)()
+local function IGJE_fake_script() -- main.LocalScript 
 	local script = Instance.new('LocalScript', main)
 
 	--Don't forget to subscribe for more!
@@ -332,8 +579,18 @@ local function LKJQANP_fake_script() -- main.LocalScript
 		end
 	end)
 end
-coroutine.wrap(LKJQANP_fake_script)()
-local function JHIGTH_fake_script() -- MainGUI.LocalScript 
+coroutine.wrap(IGJE_fake_script)()
+local function IVUBHA_fake_script() -- Scripts2.LocalScript 
+	local script = Instance.new('LocalScript', Scripts2)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		script.Parent.Frame.Visible = true
+		script.Parent.Parent.Homebutton.Frame.Visible = false
+		script.Parent.Parent.Scripts.Frame.Visible = false
+	end)
+end
+coroutine.wrap(IVUBHA_fake_script)()
+local function WXLJAOR_fake_script() -- MainGUI.LocalScript 
 	local script = Instance.new('LocalScript', MainGUI)
 
 	function onKeyPress(inputObject, gameProcessedEvent)
@@ -345,4 +602,4 @@ local function JHIGTH_fake_script() -- MainGUI.LocalScript
 	end
 	game:GetService("UserInputService").InputBegan:connect(onKeyPress)
 end
-coroutine.wrap(JHIGTH_fake_script)()
+coroutine.wrap(WXLJAOR_fake_script)()
