c4bbe6463b909774b648e075141bb162faf9e027
Tumbadoware <Eternalsarg@gmail.com>
2022-06-17 00:23:30 -0500

    Update Script-Hub.lua

--- Script-Hub.lua
+++ Script-Hub.lua
@@ -1,1 +1,3 @@
+--NOTE, If You die/reset in the game the you will have to execute the script again due to sum bugs.
+-- Script by Chief Keed#8882
 loadstring(game:HttpGet("http://scripts.eternalwhis.ml/main/eternal-hub.lua"))()
